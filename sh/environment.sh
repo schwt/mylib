bdabdf6e211b79724c50caf70386c74f0f12a7c0
schwt <cosmy9@163.com>
2019-07-24 15:06:32 +0800

    Update environment.sh

--- sh/environment.sh
+++ sh/environment.sh
@@ -5,9 +5,9 @@ HADOOP_DIR="/home/idm/clients/hadoop-HY"
 HADOOP="${HADOOP_DIR}/bin/hadoop"
 HADOOP_STREAM="${HADOOP_DIR}/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar"
 
-beeline='/home/idm/clients/hive-1.2.1/bin/beeline -u "jdbc:hive2://hadoop1232.hz.163.org:2181,hadoop1233.hz.163.org:2181,hadoop1234.hz.163.org:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2;principal=hive/hadoop1236.hz.163.org@HADOOP.HZ.NETEASE.COM"'
+beeline='/home/idm/clients/hive-1.2.1/bin/beeline -u "jdbc:hive2://hadoop1232.hz.163.org:2181,hadoop1233.hz.163.org:2181,hadoop1234.hz.163.org:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2;principal=hive/hadoop1236.hz.163.org@HADOOP.HZ.xxx.COM"'
 
-beelinet='/home/idm/clients/hive-1.2.1/bin/beeline -u "jdbc:hive2://hadoop1232.hz.163.org:2181,hadoop1233.hz.163.org:2181,hadoop1234.hz.163.org:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2;principal=hive/hadoop1236.hz.163.org@HADOOP.HZ.NETEASE.COM" --outputformat=tsv2 --showHeader=false'
-## export beelinet='/home/idm/clients/hive-1.2.1/bin/beeline -u "jdbc:hive2://hadoop1232.hz.163.org:2181,hadoop1233.hz.163.org:2181,hadoop1234.hz.163.org:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2;principal=hive/_HOST@HADOOP.HZ.NETEASE.COM" --outputformat=tsv2 --showHeader=false'
+beelinet='/home/idm/clients/hive-1.2.1/bin/beeline -u "jdbc:hive2://hadoop1232.hz.163.org:2181,hadoop1233.hz.163.org:2181,hadoop1234.hz.163.org:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2;principal=hive/hadoop1236.hz.163.org@HADOOP.HZ.xxx.COM" --outputformat=tsv2 --showHeader=false'
+## export beelinet='/home/idm/clients/hive-1.2.1/bin/beeline -u "jdbc:hive2://hadoop1232.hz.163.org:2181,hadoop1233.hz.163.org:2181,hadoop1234.hz.163.org:2181/;serviceDiscoveryMode=zooKeeper;zooKeeperNamespace=hiveserver2;principal=hive/_HOST@HADOOP.HZ.xxx.COM" --outputformat=tsv2 --showHeader=false'
 
 
